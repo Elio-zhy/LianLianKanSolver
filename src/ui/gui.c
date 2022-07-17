26e1087b93075a933c1293e603105194805a5273
U-DANDELION\Elio <elio-zhou@outlook.com>
2022-07-17 21:40:56 +0800

    modify ui

--- src/ui/gui.c
+++ src/ui/gui.c
@@ -9,18 +9,22 @@ static void activate(GtkApplication *app, gpointer user_data)
 {
     GtkWidget *window; // Window
     GtkWidget *grid; // Grid
+    GtkWidget *scale; // Scale
     GtkWidget *delay, *status; // Label
-    GtkWidget *delay_entry; // Entry
     GtkWidget *one_step_btn, *solve_btn, *detect_btn; // Button
-    GtkEntryBuffer *delay_buffer; // EntryBuffer
+
+    HWND *hwnd = (HWND *)malloc(sizeof(HWND));
+    Board *board = createBoard(ROWS * COLS);
+    DetectThreadData *detect_data = (DetectThreadData *)malloc(sizeof(DetectThreadData));
+    OneStepThreadData *one_step_data = (OneStepThreadData *)malloc(sizeof(OneStepThreadData));
+    SolveThreadData *solve_data = (SolveThreadData *)malloc(sizeof(SolveThreadData));
 
     // widget initialization
     window = gtk_application_window_new(app);
     grid = gtk_grid_new();
+    scale = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 2000, 100);
     delay = gtk_label_new("Delay:");
     status = gtk_label_new("waiting for game...");
-    delay_buffer = gtk_entry_buffer_new("1000", -1);
-    delay_entry = gtk_entry_new_with_buffer(delay_buffer);
     one_step_btn = gtk_button_new_with_label("one step");
     solve_btn = gtk_button_new_with_label("solve");
     detect_btn = gtk_button_new_with_label("detect");
@@ -30,7 +34,7 @@ static void activate(GtkApplication *app, gpointer user_data)
     gtk_grid_attach(GTK_GRID(grid), status, 0, 0, 3, 1);
     gtk_grid_attach(GTK_GRID(grid), detect_btn, 3, 0, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), delay, 0, 1, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), delay_entry, 1, 1, 3, 1);
+    gtk_grid_attach(GTK_GRID(grid), scale, 1, 1, 3, 1);
     gtk_grid_attach(GTK_GRID(grid), one_step_btn, 0, 2, 2, 1);
     gtk_grid_attach(GTK_GRID(grid), solve_btn, 2, 2, 2, 1);
     gtk_widget_set_margin_start(grid, 15);
@@ -41,11 +45,32 @@ static void activate(GtkApplication *app, gpointer user_data)
     gtk_grid_set_row_spacing(GTK_GRID(grid), 10);
 
     // widget properties
+    int width;
+    gtk_widget_get_size_request(status, &width, NULL);
+    gtk_widget_set_size_request(status, width, -1);
     gtk_window_set_title(GTK_WINDOW(window), "Solver");
     gtk_window_set_resizable(GTK_WINDOW(window), false);
-    gtk_entry_set_placeholder_text(GTK_ENTRY(delay_entry), "delay (ms)");
-    gtk_editable_set_editable(GTK_EDITABLE(window_entry), false);
+    gtk_scale_add_mark(GTK_SCALE(scale), 0, GTK_POS_BOTTOM, "0s");
+    gtk_scale_add_mark(GTK_SCALE(scale), 1000, GTK_POS_BOTTOM, "1s");
+    gtk_scale_add_mark(GTK_SCALE(scale), 2000, GTK_POS_BOTTOM, "2s");
 
+    // bind signal
+    detect_data->hwnd = hwnd;
+    detect_data->status = status;
+    one_step_data->hwnd = hwnd;
+    one_step_data->board = board;
+    one_step_data->rows = ROWS;
+    one_step_data->cols = COLS;
+    solve_data->hwnd = hwnd;
+    solve_data->board = board;
+    solve_data->scale = scale;
+    solve_data->rows = ROWS;
+    solve_data->cols = COLS;
+    g_signal_connect_swapped(detect_btn, "clicked", G_CALLBACK(detect_btn_clicked), (gpointer)detect_data);
+    g_signal_connect_swapped(one_step_btn, "clicked", G_CALLBACK(one_step_btn_clicked), (gpointer)one_step_data);
+    g_signal_connect_swapped(solve_btn, "clicked", G_CALLBACK(solve_btn_clicked), (gpointer)solve_data);
+
+    // show window
     gtk_widget_show(window);
 }
 
--- src/ui/gui.h
+++ src/ui/gui.h
@@ -1,1 +1,9 @@
+#ifndef GUI_H
+#define GUI_H
+
 #include <gtk/gtk.h>
+#include "api/api.h"
+#define ROWS 11
+#define COLS 19
+
+#endif
